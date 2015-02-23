f35b48160750a3c5dd2103e5888e673776fbaef5
Peng.Li <peng.li@rea-group.com>
2015-02-24 00:33:45 +0800

    Show category name in top of main page

--- src/main/scala/in/freewind/fastlinks/chrome_app/MainPage.scala
+++ src/main/scala/in/freewind/fastlinks/chrome_app/MainPage.scala
@@ -12,19 +12,29 @@ object MainPage extends TypedReactSpec with TypedEventListeners {
 
   case class Props(meta: Option[Meta] = None, appBackend: AppBackend)
 
-  override def getInitialState(self: This) = {
-    import self._
-    props.meta.map { meta =>
+  override def getInitialState(self: This) = State()
+
+  override def componentWillReceiveProps(self: This, nextProps: Props): Unit = {
+    nextProps.meta.map { meta =>
       val currentCategory = meta.categories.headOption
       val currentProject = currentCategory.flatMap(_.projects.headOption)
-      State(currentCategory = currentCategory, currentProject = currentProject)
-    }.getOrElse(State())
+      self.setState(self.state.copy(currentCategory = currentCategory, currentProject = currentProject))
+    }
+  }
+
+  implicit class Closure(self: This) {
+
+    import self._
+
+    def selectCategory(category: Category): Unit = {
+      setState(state.copy(currentCategory = Some(category)))
+    }
   }
 
   @scalax
   override def render(self: This) = {
     <div>
-      { Header(Header.Props(self.props.appBackend)) }
+      { Header(Header.Props(self.props.meta, self.selectCategory, self.props.appBackend)) }
       <div>
         {
           FastLinks(FastLinks.Props(projects = self.state.currentCategory.map(_.projects).getOrElse(Nil)))
--- src/main/scala/in/freewind/fastlinks/chrome_app/main/Header.scala
+++ src/main/scala/in/freewind/fastlinks/chrome_app/main/Header.scala
@@ -2,13 +2,16 @@ package in.freewind.fastlinks.chrome_app.main
 
 import com.xored.scalajs.react.{scalax, TypedReactSpec}
 import com.xored.scalajs.react.util.TypedEventListeners
+import in.freewind.fastlinks.{Category, Meta}
 import in.freewind.fastlinks.chrome_app.AppBackend
 
 object Header extends TypedReactSpec with TypedEventListeners {
 
   case class State()
 
-  case class Props(appBackend: AppBackend)
+  case class Props(meta: Option[Meta],
+                   selectCategory: Category => Unit,
+                   appBackend: AppBackend)
 
   override def getInitialState(self: This) = State()
 
@@ -17,12 +20,24 @@ object Header extends TypedReactSpec with TypedEventListeners {
     val goToConfigPage = button.onClick(e => {
       self.props.appBackend.goToConfigPage()
     })
+
+    def selectCategory(category: Category) = button.onClick(e => {
+      self.props.selectCategory(category)
+    })
   }
 
   @scalax
   override def render(self: This) = {
-    <div>
-      <button onClick={self.goToConfigPage}>Go to config page</button>
+    <div className="config-header">
+      {
+        self.props.meta match {
+          case Some(meta) => meta.categories.map(category =>
+            <button onClick={self.selectCategory(category)}>{category.name}</button>
+          )
+          case _ => <span>no meta</span>
+        }
+      }
+      <button onClick={self.goToConfigPage}>config</button>
     </div>
   }
 
